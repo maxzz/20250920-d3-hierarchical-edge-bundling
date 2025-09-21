f741c182e3e8fa881d9b48ccb2974cf3f42421d6
Max Zakharzhevskiy <maxzz@msn.com>
2025-09-20 18:20:20 -0700

    WIP. clean up. 2

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,20 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true
-  },
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
-  }
-})
+    build: {
+        outDir: 'dist',
+        sourcemap: true
+    },
+    resolve: {
+        alias: {
+            '@': '/src'
+        }
+    },
+    server: {
+        port: 3000,
+        open: true
+    },
+    plugins: [react()],
+});
