5b79934582bd8d787a659dd80e2784aabd00e1d7
Max Zakharzhevskiy <maxzz@msn.com>
2025-09-20 19:00:27 -0700

    WIP. clean up. 7. connect tailwind v4

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,7 +15,10 @@ export default defineConfig({
     },
     server: {
         port: 3000,
-        open: true
+        open: false,
     },
-    plugins: [react()],
+    plugins: [
+        react(),
+        tailwindcss(),
+    ],
 });
