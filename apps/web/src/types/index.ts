2b6f2816579ad5a71bf70576bfa91a0551687187
Max Zakharzhevskiy <maxzz@msn.com>
2025-09-20 18:21:41 -0700

    WIP. clean up. 3. fix postcss commonjs

--- apps/web/src/types/index.ts
+++ apps/web/src/types/index.ts
@@ -1,16 +1,16 @@
 export interface NodeData {
-  name: string;
-  group: number;
-  children?: NodeData[];
+    name: string;
+    group: number;
+    children?: NodeData[];
 }
 
 export interface LinkData {
-  source: string;
-  target: string;
-  value: number;
+    source: string;
+    target: string;
+    value: number;
 }
 
 export interface HierarchicalData {
-  nodes: NodeData[];
-  links: LinkData[];
+    nodes: NodeData[];
+    links: LinkData[];
 }
