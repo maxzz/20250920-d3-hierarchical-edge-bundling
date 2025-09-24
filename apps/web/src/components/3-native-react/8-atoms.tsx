59b9ac90306628b6b6af5dd3810aaa55d23ef297
Max Zakharzhevskiy <maxzz@msn.com>
2025-09-23 18:51:34 -0700

    WIP. d3-monolith. init-data. new model

--- apps/web/src/components/3-native-react/8-atoms.tsx
+++ apps/web/src/components/3-native-react/8-atoms.tsx
@@ -2,19 +2,50 @@ import { atom } from "jotai";
 import { type HierarchicalData } from "@/store/9-types";
 import { type OurNode, type OurLink } from "./9-types-internal";
 import { createAllNodesAndLinks } from "./3-create-all-nodes-and-links";
+import type { Atom } from "jotai";
+import type { Atomize } from "@/utils";
 
 const _dataAtom = atom<HierarchicalData>({
     nodes: [],
     links: [],
 });
 
+type DataModel = {
+    data: HierarchicalData;
+    // allNodes: OurNode[];
+    // linkData: OurLink[];
+    centerX: number;
+    centerY: number;
+    radius: number;
+};
+
+export const dataModel: Atomize<DataModel> = {
+    dataAtom: atom<HierarchicalData>({
+        nodes: [],
+        links: [],
+    }),
+    // allNodesAtom: atom<OurNode[]>([]),
+    // linkDataAtom: atom<OurLink[]>([]),
+    centerXAtom: atom(0),
+    centerYAtom: atom(0),
+    radiusAtom: atom(0),
+};
+
 export const dataAtom = atom((get) => get(_dataAtom), _SetDataAtom);
-export const allNodesAtom = atom<OurNode[]>([]);
-export const linkDataAtom = atom<OurLink[]>([]);
 
 function _SetDataAtom(get: Getter, set: Setter, newData: HierarchicalData): void {
     const { allNodes, linkData } = createAllNodesAndLinks(newData);
-    set(allNodesAtom, allNodes);
+    //set(allNodesAtom, allNodes);
     set(linkDataAtom, linkData);
     set(_dataAtom, newData);
 }
+
+export const allNodesAtom = atom(
+    (get) => {
+        const { allNodes } = createAllNodesAndLinks(get(dataModel.dataAtom));
+        // const rv;
+        return allNodes;
+    },
+);
+
+export const linkDataAtom = atom<OurLink[]>([]);
