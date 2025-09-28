4e2b39f885e4015f0a768097c2c6b7df885bf194
Max Zakharzhevskiy <maxzz@msn.com>
2025-09-27 18:35:10 -0700

    WIP. add second demo place holder

---
+++ apps/web/src/components/2-hierarchical-edge-bundling/1-native-d3-monolith/1m-hierarchical-edge-bundling.tsx
@@ -0,0 +1,25 @@
+// import { type HTMLAttributes, useEffect, useRef } from "react";
+// import { classNames } from "../../../utils";
+// import { type HierarchicalData } from "../../../store/9-types";
+// import { updateD3Diagram } from "./2m-update-d3-diagram";
+
+// export function HierarchicalEdgeBundlingMonolith({ data, className, ...rest }: { data: HierarchicalData; } & HTMLAttributes<SVGSVGElement>) {
+//     const svgRef = useRef<SVGSVGElement>(null);
+
+//     const width = 800;
+//     const height = 600;
+
+//     useEffect(
+//         () => updateD3Diagram({ data, width, height, svgRef }),
+//         [data, width, height]
+//     );
+
+//     return (
+//         <svg
+//             ref={svgRef}
+//             viewBox={`0 0 ${width} ${height}`}
+//             className={classNames("size-full border bg-white border-gray-200 rounded-lg shadow-sm", className)}
+//             {...rest}
+//         />
+//     );
+// }
